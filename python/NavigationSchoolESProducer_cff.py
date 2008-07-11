d359259fe034b1e726ffd96d307798c2c92caefa
cmsbuild <>
2008-07-11 14:38:23 +0000

    This commit was manufactured by cvs2git to create tag 'RecoTracker- TkNavigation-V02-06-09'.
    Sprout from master 2008-05-03 11:42:30 UTC Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch> 'fix to 2-vector dot product.'
    Cherrypick from master 2008-07-11 14:38:12 UTC Shahram Rahatlou <sha1-ca1514f63de30935a6ecb085b6242dc5bad3fd8f@cern.ch> 'remove importing pythin fragments provided in Configuration.StandardSequences.Geoemtry_cff. The latter should be included in all high level configuration files':
        data/NavigationSchoolESProducer.cff
        python/NavigationSchoolESProducer_cff.py

--- python/NavigationSchoolESProducer_cff.py
+++ python/NavigationSchoolESProducer_cff.py
@@ -1,11 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 # initialize magnetic field #########################
-from MagneticField.Engine.volumeBasedMagneticField_cfi import *
 # initialize geometry #####################
-from Geometry.CMSCommonData.cmsIdealGeometryXML_cfi import *
-from Geometry.TrackerGeometryBuilder.trackerGeometry_cfi import *
-from Geometry.TrackerNumberingBuilder.trackerNumberingGeometry_cfi import *
 from RecoTracker.GeometryESProducer.TrackerRecoGeometryESProducer_cfi import *
 # navigationschools
 from RecoTracker.TkNavigation.NavigationSchoolESProducer_cfi import *
